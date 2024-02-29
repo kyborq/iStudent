e6fd88f97d9669c99f55e793fe3b9eb94972266f
Konstantin <k.podyganov@mail.ru>
2024-02-29 10:19:15 +0300

    fix: query client fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from 'react-native';
+import { NativeModules, StatusBar } from 'react-native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppNavigation } from './components/Navigator';
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export const App = () => {
   return (
--- app/src/api/api.ts
+++ app/src/api/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://192.168.246.210:3000';
+export const BASE_URL = 'http://192.168.96.210:3000';
 // export const BASE_URL = 'http://10.0.2.2:3000';
 // export const BASE_URL = 'http://127.0.0.1:3000';
 
 export const api = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
+  withCredentials: false,
 });
